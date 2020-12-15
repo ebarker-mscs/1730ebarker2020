bb257b650faed2a25bf4c53733e8e8b6ccdb80da
Elizabeth Barker <zw5845yk@go.minnstate.edu>
2020-12-14 21:10:32 -0600

    exercise 3c done

---
+++ ebarker1730ex3c/Ex3cCalculations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ebarker1730ex3c
+{
+     public class Ex3cCalculations
+    {
+        public static string Calc0(int index)
+        {
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            if (index >= 1 && index < 8)
+                return days.ToString();
+
+            return "Invalid index";
+        }
+
+        public static string Calc1(string index)
+        {
+            string[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+            string[] hours = { "Closed", "10am - 6pm", "10am - 6pm", "10am - 6pm", "10am - 9pm", "10am - 6pm", "8am - 4pm" };
+
+            return "";
+        }
+
+        public static int Calc2(int[] index)
+        {
+            return 0;
+        }
+        public static double Calc3(double[] index1, int index2)
+        {
+            return 0.0;
+        }
+        public static double Calc5(double[] index)
+        {
+            return 0.0;
+        }
+
+        public static double[] Calc6(double[] numbers)
+        {
+            return new double[10];
+        }
+    }
+}
---
+++ ebarker1730ex3cUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ebarker1730ex3cUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ebarker1730ex3cUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a269b54-e2d9-4cbc-918c-dff079abc2cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
